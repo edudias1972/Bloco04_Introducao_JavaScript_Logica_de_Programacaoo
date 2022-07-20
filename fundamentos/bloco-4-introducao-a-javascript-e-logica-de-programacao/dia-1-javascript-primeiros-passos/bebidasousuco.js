56ba9c1e0c4076f73a9d9b33453d5bfd0b62880b
edudias1972 <edudias1972@gmail.com>
2022-07-20 20:52:55 -0300

    Exemplos e exercicios JS

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/bebidasousuco.js
@@ -0,0 +1,10 @@
+//  Café OU Suco eis a questão 
+
+const bebidaPrincipal = 'cafezinho';
+const bebidaAlternativa = 'suco de laranja';
+
+if (bebidaPrincipal === 'cafezinho' || bebidaAlternativa === 'suco de laranja') {
+    console.log("Obrigado por me atender :D")
+} else {
+    console.log("Ei, eu não pedi isso!");
+}
