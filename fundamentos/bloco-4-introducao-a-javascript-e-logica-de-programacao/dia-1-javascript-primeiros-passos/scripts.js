f277c35097026b517e504a5a1554cebc4b6ef787
edudias1972 <edudias1972@gmail.com>
2022-07-20 16:24:45 -0300

    Adicionando primeiros exercicios bloco4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,22 @@
+//**Variaveis */
+console.log("Exemplo de variaveis 01");
+let age = 20;
+let name = "Hulk";
+
+console.log(age, name);
+
+console.log("Exemplo de variaveis 02 passando valores");
+/*passando valores */
+let age1 = 20;
+age1 = 30;
+let myName = "Hulk";
+
+console.log(age1, myName);
+
+console.log("Exemplo de variaveis 03 do tipo");
+const constante não reatribui valores ");
+const age2 = 20;
+age1 = 30; /**Constante não reatribui valores  */
+let myName1 = "Hulk";
+
+console.log(age2, myName1);
