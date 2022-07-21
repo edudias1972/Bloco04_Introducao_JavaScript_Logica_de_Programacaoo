d0c902f5fb239c53381b884b5206749964a80307
edudias1972 <edudias1972@gmail.com>
2022-07-21 19:01:35 -0300

    terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-JavaScript-ArrayeloopFor/praticar-JavaScript-Array-Loop3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-JavaScript-ArrayeloopFor/praticar-JavaScript-Array-Loop3.js
@@ -1,6 +1,12 @@
-/*Exercício 3
-Adicione o valor "Contato" no final do array menu:*/
+/*3)Para o terceiro exercício, calcule e imprima a média aritmética dos valores 
+contidos no array; */
 
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-menu.push('Contato');
-console.log(menu);
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+var soma = 0;
+for (let index = 0; index < numbers.length; index++) {
+    soma += numbers[index];
+    soma / numbers.length;
+    let media = soma / numbers.length;
+}
+console.log(media);
