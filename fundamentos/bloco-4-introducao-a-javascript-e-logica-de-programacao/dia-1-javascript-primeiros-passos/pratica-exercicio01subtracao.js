97ada7aee55638cba220c6ff7b53ef957278c4d3
edudias1972 <edudias1972@gmail.com>
2022-07-21 10:48:19 -0300

    Pratica Exercicio 1 primeira parte concluido

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/pratica-exercicio01subtracao.js
@@ -0,0 +1,18 @@
+/*Faça cinco programas, um para cada operação aritmética básica. 
+Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. 
+Faça programas para:
+Adição (a + b)         ok
+Subtração (a - b)      ok
+Multiplicação (a * b)
+Divisão (a / b)
+Módulo (a % b)
+*/
+
+/**Subtração*/
+
+const a = 10;
+const b = 2;
+
+const subtracao = a - b;
+
+console.log(subtracao);
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/pratica-exercicio1-modulo.js
@@ -0,0 +1,16 @@
+/*Faça cinco programas, um para cada operação aritmética básica. 
+Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. 
+Faça programas para:
+Adição (a + b)         ok
+Subtração (a - b)      ok
+Multiplicação (a * b)  ok
+Divisão (a / b)        ok
+Módulo (a % b)
+*/
+
+/**Módulo */
+
+const a = 10;
+const b = 2;
+
+const modulo = a % b;
