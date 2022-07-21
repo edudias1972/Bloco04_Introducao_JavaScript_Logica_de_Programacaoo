fe72fb71e1418275f01872c38183650abff6f15f
edudias1972 <edudias1972@gmail.com>
2022-07-21 19:13:15 -0300

    JavaScript - Array e loop For exercicio 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-JavaScript-ArrayeloopFor/praticar-JavaScript-Array-Loop3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-JavaScript-ArrayeloopFor/praticar-JavaScript-Array-Loop3.js
@@ -1,5 +1,7 @@
 /*3)Para o terceiro exercício, calcule e imprima a média aritmética dos valores 
 contidos no array; */
+/* Dica A média aritmética é o resultado da soma de todos os elementos 
+  divido pelo número total de elementos. */
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
@@ -7,6 +9,7 @@ var soma = 0;
 for (let index = 0; index < numbers.length; index++) {
     soma += numbers[index];
     soma / numbers.length;
+
     let media = soma / numbers.length;
 }
 console.log(media);
