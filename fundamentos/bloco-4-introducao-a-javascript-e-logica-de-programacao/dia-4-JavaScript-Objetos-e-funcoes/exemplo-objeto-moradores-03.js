b5be8bd3d20629551c717d4e8427f7ac5df4d7a2
edudias1972 <edudias1972@gmail.com>
2022-07-25 19:36:34 -0300

    JavaScrit Aula trabalhando com Objeto exemplos e exercícios

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-JavaScript-Objetos-e-funcoes/exemplo-objeto-moradores-03.js
@@ -0,0 +1,38 @@
+let moradores = [{
+        nome: 'Luiza',
+        sobrenome: 'Guimarães',
+        andar: 10,
+        apartamento: 1005,
+    },
+    {
+        nome: 'William',
+        sobrenome: 'Albuquerque',
+        andar: 5,
+        apartamento: 502,
+    },
+    {
+        nome: 'Murilo',
+        sobrenome: 'Ferraz',
+        andar: 8,
+        apartamento: 804,
+    },
+    {
+        nome: 'Zoey',
+        sobrenome: 'Brooks',
+        andar: 1,
+        apartamento: 101,
+    },
+];
+
+let primeiroMorador = moradores[0];
+console.log(primeiroMorador); // { nome: 'Luiza', sobrenome: 'Guimarães', andar: 10, apartamento: 1005 }
+console.log(primeiroMorador['andar']); // 10
+
+let segundoMorador = moradores[1];
+console.log(segundoMorador);
+console.log(segundoMorador['andar'
+    'apartamento']);
+
+let ultimoMorador = moradores[moradores.length - 1];
+console.log(ultimoMorador); // { nome: 'Zoey', sobrenome: 'Brooks', andar: 1, apartamento: 101 }
+console.log(ultimoMorador.nome); // Zoey
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-JavaScript-Objetos-e-funcoes/exercicio-1.js
@@ -0,0 +1,80 @@
+/*Objetos
+Você já aprendeu que as variáveis em JavaScript são recipientes ou containers para os dados. Por exemplo:
+*/
+//let car = "Fiat";
+//console.log(car);
+/*
+Agora é hora de aprender sobre objetos!
+Um objeto em JavaScript é uma entidade independente, com propriedades e tipos. 
+Uma propriedade é a associação entre uma chave e um valor, são essas as características do objeto.
+Pense em um objeto da vida real, como o carro do exemplo acima. 
+Cada carro tem sua cor, peso, tamanho, quantidade de portas e etc. 
+Essas são suas características, suas propriedades. 
+E, da mesma forma que funciona um objeto da vida real, funcionam os objetos em JavaScript.
+Veja o vídeo abaixo para entender melhor o conceito de objetos:
+*/
+
+let car = {
+    type: 'Fiat',
+    model: '500',
+    color: 'white',
+};
+
+console.log(car.type);
+
+console.log(car[type]);
+/*
+Para acessar as chaves dentro de um objeto, temos duas formas:
+notação de ponto: car.type
+notação de colchetes: car[type]
+Se o objeto está dentro de um array, ele é acessado através do seu índice, 
+da mesma forma que se acessaria uma informação num array de strings. 
+As chaves de um objeto são armazenadas como strings e, 
+para conseguir acessar propriedades nomeadas com números, como 0, 
+por exemplo, só é possível usando a notação de colchetes. 
+Se você usar a notação de ponto, um erro será retornado. 
+
+Ao usar a notação de colchetes também é possível chamar variáveis que 
+fazem relação com a chave que você quer acessar dentro desses colchetes.
+As chaves de um objeto são armazenadas como strings e, para conseguir acessar propriedades 
+nomeadas com números, como 0, por exemplo, só é possível usando a notação de colchetes. 
+Se você usar a notação de ponto, ocorre um erro. Ao usar a notação de colchetes também 
+é possível chamar variáveis que fazem relação com a chave que você quer acessar dentro desses colchetes.
+*/
+
+let diasDaSemana = {
+    'domingo',
+    'segunda',
+    'terça',
+    'quarta',
+    'quinta',
+    'sexta',
+    'sábado',
+};
+
+//diasDaSemana .1; // SyntaxError: Unexpected number
+console.log(diasDaSemana['1']); // domingo
+
+//Outro exemplo
+
+let conta = {
+    agencia: '0000',
+    banco: {
+        cod: '012',
+        id: 4,
+        nome: 'TrybeBank',
+    }
+};
+
+let infoDoBanco = 'banco';
+console.log(conta[infoDoBanco]); // { cod: '012', id: 4, nome: 'TrybeBank' }
+console.log(conta[infoDoBanco]['nome']); // TrybeBank
+
+console.log(conta.agencia); // 0000
+console.log(conta['agencia']); // 0000
+
+console.log(conta.banco.cod); // 012
+console.log(conta['banco']['id']); // 4
+
+console.log(conta.banco.nome);
+console.log(conta['banco']['nome']);
